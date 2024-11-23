b4d387b24c2c6e52cb2332a8798ab8a690c5ad0f
hmaiajunior <juniorce04@gmail.com>
2024-11-23 09:16:23 -0300

    adicionando explicacao sobre o uso do for

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-x = int(input("Please enter an integer: "))
+# x = int(input("Please enter an integer: "))
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+
+# for i in range(1,10):
+#     print(i)
+
+lista_alunos = ["rafael","luciano","henrique"]
+
+for i in lista_alunos:
+    print(i)
