80ccd61dbd0bfafb1bde9a018bc520404d49b724
hmaiajunior <juniorce04@gmail.com>
2024-11-27 21:43:41 -0300

    resolucao exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,10 @@
-# Exercício 01: Verificação de Qualidade de Dados
+### Exercício 1: Verificação de Qualidade de Dados
+# Você está analisando um conjunto de dados de vendas e precisa garantir 
+# que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
+# forem positivos ou "Dados inválidos" caso contrário.
 
-#Você está analisando um conjunto de dados de vendas e precisa garantir que todos os registros tenham valores positivos para quantidade e preço. Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos forem positivos ou "Dados inválidos" caso contrário.
+
 
 # quantidade = 120
 # preco = 10
@@ -17,21 +21,54 @@
 
 
 
-# Exercício 06:  Contagem de Palavras em Textos
+### Exercício 6. Contagem de Palavras em Textos
+# Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
 
-# Dado um texto, contar quantas vezes cada palavra única aparece nele
+
+# texto = "o rato roeu a roupa do rei de roma"
+# palavras_tratadas = texto.replace(",","").split()
+
+# contagem_palavras = {}
+
+# for palavra in palavras_tratadas:
+#     if palavra in contagem_palavras:
+#         contagem_palavras[palavra] += 1
+#     else:
+#         contagem_palavras[palavra] = 1
+
+# print(contagem_palavras)
 
 
-texto = "o rato roeu a roupa do rei de roma em roma"
-palavras_tratadas = texto.replace(",","").split()
-print(palavras_tratadas)
 
-contagem_palavras = {}
+### Exercício 3: Filtragem de Logs por Severidade
+# Você está analisando logs de uma aplicação e precisa filtrar mensagens 
+# com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
+# como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
+# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
-for palavra in palavras_tratadas:
-    if palavra in contagem_palavras:
-        contagem_palavras[palavra] += 1
+
+
+### Exercício 10. Agregação de Dados por Categoria
+# Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+
+
+vendas = [
+    {"categoria": "eletronicos", "valor": 1200},
+    {"categoria": "livros", "valor": 200},
+    {"categoria": "eletronicos", "valor": 800},
+    {"categoria": "eletronicos", "valor": 400},
+    {"categoria": "livros", "valor": 250},
+]
+
+vendas_categoria = {}
+
+for venda in vendas:
+    categoria = venda["categoria"]
+    valor = venda["valor"]
+    if categoria in vendas_categoria:
+        vendas_categoria[categoria] += valor
     else:
-        contagem_palavras[palavra] = 1
+        vendas_categoria[categoria] = valor
 
-print(contagem_palavras)
+    print(vendas_categoria)
+
